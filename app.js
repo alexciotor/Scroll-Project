1415f638bc7a7f52397cf6044ecb56c6edff6061
alexciotor <ciotor.alex@yahoo.com>
2020-11-02 13:15:18 +0200

    Second Commit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-$(function(){
-    $('.btn').on('click',function(){
-        $('.menuItems').slideToggle();
-         
-        
-      
-    })
-})
+// $(function(){
+//     $('.btn').on('click',function(){
+//         $('.menuItems').slideToggle('slow');
+
+//     })
+// })
+
+
+ 
+ const button = document.querySelector('.btn')
+ const menuItems =document.querySelector('.menuItems');
+ const navbar = document.querySelector('.navbar')
+
+ button.addEventListener('click',()=>{
+menuItems.classList.toggle('menuItems-toggle')
+
+
+ })
