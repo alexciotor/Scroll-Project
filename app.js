8c1bc581b7432d0d634b7935cc1c0476e9d943ac
alexciotor <ciotor.alex@yahoo.com>
2020-11-03 13:16:00 +0200

    improved method

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
-// $(function(){
-//     $('.btn').on('click',function(){
-//         $('.menuItems').slideToggle('slow');
+//   background-color: rgba(26, 63, 65, 0.733);
 
-//     })
+
+
+const nav =document.querySelector('.navbar')
+const burger = document.querySelector('.burger');
+const navElements =document.querySelector('.nav-content')
+const fistStecion =document.querySelector('.section1')
+ const header =document.querySelector('.head')
+ const Elements =document.querySelector('.elements')
+
+// window.addEventListener('scroll', ()=>{
+//     header.classList.add('header')
 // })
 
 
- 
- const button = document.querySelector('.btn')
- const menuItems =document.querySelector('.menuItems');
- const navbar = document.querySelector('.navbar')
 
- button.addEventListener('click',()=>{
-menuItems.classList.toggle('menuItems-toggle')
+burger.addEventListener('click',function(){
+const height = navElements.getBoundingClientRect().height
+const elementHeight = Elements.getBoundingClientRect().height
+ if(height===0){
+navElements.style.height = `${elementHeight}px`
+ }
+ else{
+     navElements.style.height=0;
+ }
+
+    //  if(navElements.style.maxHeight){
+    //  navElements.style.maxHeight = null; 
+    //  }
+    //  else{
+    //    navElements.style.maxHeight= navElements.scrollHeight +'px'
+    //  } 
+    //  console.log(navElements.style.maxHeight)
+ })
+
+
+ window.addEventListener('scroll', function(){
+    const scrollHeight =window.pageYOffset;
+    const navheight = nav.getBoundingClientRect().height
+    if(scrollHeight>navheight){
+        header.classList.add('header')
+    }
+    else{
+        header.classList.remove('header')
+    }
+
 
 
  })
